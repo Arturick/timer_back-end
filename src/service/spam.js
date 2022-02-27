9713696885e1a0c0279f3e2b290636c6e68ed345
Arturick <asunov.artur.2007@gmail.com>
2022-02-28 02:16:38 +0300

    a

--- src/service/spam.js
+++ src/service/spam.js
@@ -2,26 +2,32 @@ const spamModel = require('../module/spam');
 
 const spamLog = {
     async checkLogin(ip){
+        let answ = 0;
         await spamModel.select(ip ,"login")
             .then(answer => {
                if(answer[0][0]['count(*)'] > 10){
+                   answ = 1;
                    return null;
                }
 
             });
+        if(answ==1)return null;
         await spamModel.add(ip, 'login')
         await spamModel.delete(ip, 'login')
-
+        return true;
                 },
     async checkChange(ip){
+        let answ = 0;
         await spamModel.select(ip ,"change")
             .then(answer => {
                 console.log(answer[0][0]['count(*)'] > 10);
-                if(answer[0][0].count > 10){
+                if(answer[0][0]['count(*)'] > 10){
+                    answ = 1;
                     return null;
                 }
 
             });
+        if(answ==1)return null;
         await spamModel.add(ip, "change")
         await spamModel.delete(ip, "change")
         return true;
